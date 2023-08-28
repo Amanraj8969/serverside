554be909f4b5b665c9ea4f3a9aaa6cd843ccac42
Aman Kumar <96868781+Amanraj8969@users.noreply.github.com>
2023-08-28 10:17:30 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const fs = require("fs");
 
 // MongoDB
 mongoose
-  .connect("{paste your mongodb url}", {
+  .connect("mongodb+srv://Nebula:12345@cluster0.mlfyqxq.mongodb.net/?retryWrites=true&w=majorit", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
